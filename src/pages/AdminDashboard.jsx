af341145d98900a8fb4f6200292e01f3ed73c90c
Smrithi-l <lsmrithi1425@gmail.com>
2025-03-26 00:23:35 +0530

    first commit

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1028,7 +1028,7 @@ const AdminDashboard = () => {
                           <TableCell>{user.email}</TableCell>
                           <TableCell>
                             <StatusChip
-                              label={user.role.charAt(0).toUpperCase() + user.role.slice(1)}
+                              label={user.role.toUpperCase() + user.role.slice(1)}
                               status={user.role === "admin" ? "Admin" : "Employee"}
                               size="small"
                             />
