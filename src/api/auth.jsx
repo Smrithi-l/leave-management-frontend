1971ee89d389d3a8b21b5fbc1ac1d369476e3804
Smrithi-l <lsmrithi1425@gmail.com>
2025-03-25 23:58:33 +0530

    first commit

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; 
+const API_URL = "https://leave-management-backend-sa2e.onrender.com/api/auth"; 
 export const loginUser = async (email, password) => {
     const response = await axios.post(`${API_URL}/login`, { email, password });
     return response.data;
